954549453538bf24a602858f0974d31356cb2efd
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-12-16 00:18:25 +0100

    Add __init__.py
    To make this repo installable as Python package via package_dir
    redirection, with its contents as package_data

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# All files under this package directory are distributed under the terms of
+# the Modified BSD License.
+
+"""
+Only declaring this directory as a Python package.
+
+To make the directory contents directly installable as ``package_data`` of a
+Python package redirected to this via ``package_dir`` in a ``setup.py`` of a
+Python project that wants to bundle ``declarativewidgets_explorer`` in a
+simple way
+"""
